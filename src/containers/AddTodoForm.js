9c84628277705d46eaa50e1da03f539aea443e91
Bailey Dunning <bwdunning@gmail.com>
2021-01-31 10:58:37 -0700

    add todo list container

--- src/containers/AddTodoForm.js
+++ src/containers/AddTodoForm.js
@@ -30,4 +30,8 @@ class AddTodoForm extends Component {
   }
 }
 
-export default AddTodoForm
+const mapDispatchToProps = dispatch => ({
+  addToDo: text => dispatch(addToDo(text))
+})
+
+export default connect(null, mapDispatchToProps)(AddTodoForm)
--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,7 +1,7 @@
 export const todos = (state = [], action) => {
   switch (action.type) {
     case 'ADD_TODO':
-      return [...state, { id: Date.now(), text: action.todo, completed: false }]
+      return [...state, { id: Date.now(), todo: action.todo, completed: false }]
     default:
       return state
   }
